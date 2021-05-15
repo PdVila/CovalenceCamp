7013d6aca60307c03a17e95779e69e436d30356c
Peter Vila <42393951+PdVila@users.noreply.github.com>
2021-05-14 18:27:26 -0700

    Add files via upload

---
+++ 99 Lines of Code/99 Lines of Code/script.js
@@ -0,0 +1,14 @@
+let friends = [];
+
+friends.push('Tommy','Joel','Abe','Jenny','Candice')
+console.log(friends);
+
+//For now we aren't changing the page but console logging our song
+
+console.log(`And the song begins~`)
+
+for (let j = 0; j < friends.length; j++) {
+    for(let i = 99; i > 0; i--) {
+        console.log(`${[i]} lines of code in the file, ${[i]} lines of code; ${[friends[j]]} strikes one out, clears it out, ${[i]} lines of code in the file.]`)
+    }
+} //497 MESSAGES IN THE CONSOLEEEE 
